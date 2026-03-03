5e1be6ba4d3defa9523679c100a94723228cdab6
Daniel Mroczka <daniel.mroczka@gmail.com>
2026-03-03 21:45:47 +0100

    initial commit

--- redirect.js
+++ redirect.js
@@ -1,7 +1,5 @@
 (function() {
-    const lastVisited = localStorage.getItem('gcp_exam_last_visited');
-    const defaultPage = 'q_001.html';
-
-    // Przekieruj do ostatniej strony lub do domyślnej
+    const lastVisited = localStorage.getItem('gcp_devops_exam_last_visited');
+    const defaultPage = 'question_001.html';
     window.location.href = lastVisited ? lastVisited : defaultPage;
 })();
