63d053c50f5d851fd2e5bb78870a097d24ada1b3
Daniel Mroczka <daniel.mroczka@gmail.com>
2026-06-23 11:49:55 +0200

    Update navigation links to include ignore_last parameter and track last visited question in local storage

--- redirect.js
+++ redirect.js
@@ -1,5 +1,18 @@
 (function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.has('ignore_last')) {
+        return; // Stay on index.html
+    }
     const lastVisited = localStorage.getItem('gcp_devops_exam_last_visited');
     const defaultPage = 'q_001.html';
-    window.location.href = lastVisited ? lastVisited : defaultPage;
+    if (lastVisited && lastVisited !== 'index.html') {
+        window.location.href = lastVisited;
+    } else {
+        // If we're already on index.html and no lastVisited, 
+        // we might want to go to q_001.html by default, 
+        // but let's only redirect if we're at the root index.
+        if (!urlParams.has('ignore_last')) {
+             window.location.href = defaultPage;
+        }
+    }
 })();
